de4579f9309a9bb7670c62e07591133a73479593
calvinwilliams <calvinwilliams@163.com>
2019-04-16 23:56:31 +0800

    UPDATE TO V0.2.0.0E

---
+++ src/test/java/xyz/calvinwilliams/sqlaction/pp.bat
@@ -0,0 +1,2 @@
+java -Dfile.encoding=UTF-8 -classpath "D:\Work\mysql-connector-java-8.0.15\mysql-connector-java-8.0.15.jar;%USERPROFILE%\.m2\repository\xyz\calvinwilliams\okjson\0.0.9.0\okjson-0.0.9.0.jar;%USERPROFILE%\.m2\repository\xyz\calvinwilliams\sqlaction\0.2.0.0\sqlaction-0.2.0.0.jar" xyz.calvinwilliams.sqlaction.SqlActionGencode
+pause
